32d8ccf0dd6c8b6d445170e787b76c7b4774a7e9
peter.kehl <peter.kehl@gmail.com>
2014-06-12 14:50:42 +1000

    Setup proxy for Win

---
+++ setup_proxy.bat
@@ -0,0 +1,20 @@
+@echo off
+for /f "tokens=*" %%a in ('dir  %APPDATA%\Mozilla\Firefox\Profiles\*.default /b') do set p=%APPDATA%\Mozilla\Firefox\Profiles\%%a
+if defined p (
+    set e=%p%\extensions
+    REM If you have not got any extensions in Firefox profile yet, there is no 'extensions' folder. So create it.
+    if not exist %e% (
+      mkdir %e%
+    )
+    
+    REM Now set up the actual extensions, unless they are already installed from XPI    
+    REM Do not use: echo %CD%.>target-file. Use: cd >target-file. For some reason %CD% doesn't get updated after I change directory.
+    if not exist %e%\selblocks-global@selite.googlecode.com.xpi (
+      cd src
+      cd > %e%\selblocks-global@selite.googlecode.com
+      cd ..\..
+    )
+
+) else (
+   echo Could not find a default Firefox profile
+)
