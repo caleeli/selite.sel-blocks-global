9a0431c46822555b1481a07344b1f70dba17ac69
peter.kehl <peter.kehl@gmail.com>
2014-06-17 16:07:02 +1000

    mproxy

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -2,6 +2,8 @@
 #change dir to where this script is run from:
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+# Based on https://developer.mozilla.org/en/Setting_up_extension_development_environment and http://kb.mozillazine.org/Profile_folder_-_Firefox
+
 cd src
 
 # TODO: Make this script accept an optional parameter, which is a name of Firefox profile`
@@ -9,23 +11,22 @@ HOME_FOLDER=~
 
 if [ "$(uname)" == "Darwin" ]; then
    # According to http://kb.mozillazine.org/Profile_folder_-_Firefox  there are two places for Firefox profile folder on Mac OS:
-   # ~/Library/Mozilla/Firefox/Profiles/<profile folder> or ~/Library/Application Support/Firefox/Profiles/<profile folder> 
-   # But on Mac OS 10.5.8 I could see the second folder only. If you can test both, please update this/send this to me.
-   # Anyway, I don't know how to escape this for now, so if you know, fix this.
-   EXTENSION_FOLDER=$HOME_FOLDER/"'Application Support/Firefox/Profiles/*.default'"
+   # ~/Library/Mozilla/Firefox/Profiles/<profile folder> or ~/Library/Application Support/Firefox/Profiles/<profile folder>
+   # But on Mac OS 10.5.8 and 10.9.1 I could see the second folder only. If you can test both, please update this/send this to me.
+   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/Library/Application Support/Firefox/Profiles"/*.default )"
 else
-   EXTENSION_FOLDER="$HOME_FOLDER/.mozilla/firefox/*.default"
+   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/.mozilla/firefox/"*.default )"
 fi
-EXTENSION_FOLDER=`echo $EXTENSION_FOLDER`/extensions
+EXTENSION_FOLDER=$EXTENSION_FOLDER/extensions
 
 # If you haven't got any extensions in Firefox profile yet, there is no 'extensions' folder. So create it.
-if [ ! -e $EXTENSION_FOLDER ]
+if [ ! -e "$EXTENSION_FOLDER" ]
 then
-  mkdir $EXTENSION_FOLDER
+  mkdir "$EXTENSION_FOLDER"
 fi
 
-if [ ! -e $EXTENSION_FOLDER/selblocks-global\@selite.googlecode.com.xpi ]
+if [ ! -e "$EXTENSION_FOLDER"/selblocks-global\@selite.googlecode.com.xpi ]
 then
-  pwd > $EXTENSION_FOLDER/selblocks-global@selite.googlecode.com
+  pwd > "$EXTENSION_FOLDER"/selblocks-global@selite.googlecode.com
 fi
 
