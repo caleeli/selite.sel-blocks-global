42e8a21e2e0ed0064be0007203cb2e789d9161ca
peter.kehl <peter.kehl@gmail.com>
2014-11-28 11:36:19 +1100

    Setup_proxies

--- setup_proxy.bat
+++ setup_proxy.bat
@@ -1,19 +1,24 @@
 @echo off
-REM Before invoking this script, run 'cd <folder where this script is>'
-FOR /D %%c in (%APPDATA%\Mozilla\Firefox\Profiles\*.default) DO set p=%%c
+REM Following is for expansion of variables at runtime - e.g. !e! instead of %e%
+setlocal EnableDelayedExpansion
+SET script_folder=%~dp0
+cd %script_folder:~0,-1%
+
+FOR /D %%c in ("%APPDATA%\Mozilla\Firefox\Profiles\*.default") DO set p=%%c
 if defined p (
-    set e=%p%\extensions
+    set e="%p%\extensions"
     REM If you have not got any extensions in Firefox profile yet, there is no 'extensions' folder. So create it.
-    if not exist %e% (
-      mkdir %e%
+    if not exist "!e!" (
+      mkdir "!e!"
     )
     
     REM Now set up the actual extensions, unless they are already installed from XPI    
     REM Do not use: echo %CD%.>target-file. Use: cd >target-file. For some reason %CD% doesn't get updated after I change directory.
+    if not exist "!e!\selblocks-global@selite.googlecode.com.xpi" (
       cd src
-      cd > %e%\selblocks-global@selite.googlecode.com
+      cd > "!e!\selblocks-global@selite.googlecode.com"
       cd ..
-
+    )
 ) else (
    echo Could not find a default Firefox profile
 )
