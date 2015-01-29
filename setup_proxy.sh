d6da4edabf0c15188dc973a5a1ba00ceec330262
peter.kehl <peter.kehl@gmail.com>
2015-01-29 12:51:19 +1100

    Setting up proxies

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -3,19 +3,24 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # Based on https://developer.mozilla.org/en/Setting_up_extension_development_environment and http://kb.mozillazine.org/Profile_folder_-_Firefox
+# This script accepts an optional parameter, which is a name of Firefox profile. Otherwise it uses 'default' profile. Either way, the profile must have been created by Firefox (i.e. its folder name must be in standard format).
+if [[ "$1" ]]
+then
+    PROFILE="$1"
+else
+    PROFILE=default
+fi
+HOME_FOLDER=~
 
 cd src
-
-# TODO: Make this script accept an optional parameter, which is a name of Firefox profile`
-HOME_FOLDER=~
 
 if [ "$(uname)" == "Darwin" ]; then
    # According to http://kb.mozillazine.org/Profile_folder_-_Firefox  there are two places for Firefox profile folder on Mac OS:
    # ~/Library/Mozilla/Firefox/Profiles/<profile folder> or ~/Library/Application Support/Firefox/Profiles/<profile folder>
    # But on Mac OS 10.5.8 and 10.9.1 I could see the second folder only. If you can test both, please update this/send this to me.
-   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/Library/Application Support/Firefox/Profiles"/*.default )"
+   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/Library/Application Support/Firefox/Profiles"/*.$PROFILE )"
 else
-   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/.mozilla/firefox/"*.default )"
+   EXTENSION_FOLDER="$( echo "$HOME_FOLDER/.mozilla/firefox/"*.$PROFILE )"
 fi
 EXTENSION_FOLDER=$EXTENSION_FOLDER/extensions
 
